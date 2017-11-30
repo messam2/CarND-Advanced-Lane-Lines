5693264af7879ae2bce90e392d25ed9eaeba9150
Mohammed ESSAM <mohammed.essam@valeo.com>
2017-11-30 10:59:15 +0200

    adding find_lanes function to p4.py

--- p4.py
+++ p4.py
@@ -130,6 +130,91 @@ def unwarp(img, src, dst):
     # Return the resulting image and matrix
     return warped, M
 	
+def find_lanes(img, nwindows=9, margin=100):
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(img[img.shape[0]//2:,:], axis=0)
+    
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((img, img, img))*255
+    
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    
+    # Set height of windows
+    window_height = np.int(img.shape[0]/nwindows)
+    
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    
+    
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+    
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = img.shape[0] - (window+1)*window_height
+        win_y_high = img.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+        (0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+        (0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    
+    
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    
+    return out_img
+	
 def process_image(image):
 	mtx, dist = camera_cal('camera_cal')
 
@@ -146,41 +231,46 @@ def process_image(image):
                     [320. / 1280. * img_size[1], 720. / 720. * img_size[0]]], np.float32)
 	combined_binary_top_down, perspective_M = unwarp(combined_binary, src, dst)
 	
-	
-	output = combined_binary_top_down
+	output = find_lanes(combined_binary_top_down)
+
 	return output
 	
 	
 	
 if __name__ == "__main__":
-	video = False
+    video = False
+    
+    if video:
+        input_video = 'project_video.mp4'
+        # input_video = 'challenge_video.mp4'
+        # input_video = 'harder_challenge_video.mp4'
+        
+        white_output = 'output_videos/' + input_video.split('.mp4')[0] + '.mp4'
+        
+        # clip1 = VideoFileClip(input_video).subclip(0,5)
+        clip1 = VideoFileClip(input_video)
+        
+        white_clip = clip1.fl_image(process_image)
+        
+        white_clip.write_videofile(white_output, audio=False)
+		
+    else:
+        img = cv2.imread('signs_vehicles_xygrad.png')
+        img = cv2.imread('test_images\straight_lines1.jpg')
+        img = cv2.imread('test_images\straight_lines2.jpg')
+        
+        # output, left_fitx, right_fitx, ploty = process_image(img)
+        output = process_image(img)
 
-	if video:
-		input_video = 'project_video.mp4'
-		# input_video = 'challenge_video.mp4'
-		# input_video = 'harder_challenge_video.mp4'
-		
-		white_output = 'output_videos/' + input_video.split('.mp4')[0] + '.mp4'
-		
-		# clip1 = VideoFileClip(input_video).subclip(0,5)
-		clip1 = VideoFileClip(input_video)
-		
-		white_clip = clip1.fl_image(process_image)
-		
-		white_clip.write_videofile(white_output, audio=False)
-		
-	else:
-		img = cv2.imread('signs_vehicles_xygrad.png')
-		img = cv2.imread('test_images\straight_lines1.jpg')
-		img = cv2.imread('test_images\straight_lines2.jpg')
-		
-		output = process_image(img)
-		
-		plt.figure(figsize=(15, 15))
-		plt.subplot(1, 2, 1)
-		plt.imshow(img)
-		plt.title('Original Image')
-		plt.subplot(1, 2, 2)
-		plt.imshow(output)
-		plt.title('Ouput Image')
-		plt.show()
+        plt.figure(figsize=(15, 15))
+        plt.subplot(1, 2, 1)
+        plt.imshow(img)
+        plt.title('Original Image')
+        plt.subplot(1, 2, 2)
+        plt.imshow(output)
+        # plt.plot(left_fitx, ploty, color='yellow')
+        # plt.plot(right_fitx, ploty, color='yellow')
+        # plt.xlim(0, 1280)
+        # plt.ylim(720, 0)
+        plt.title('Ouput Image')
+        plt.show()
